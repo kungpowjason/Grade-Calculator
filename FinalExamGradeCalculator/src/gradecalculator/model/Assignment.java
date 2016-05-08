928b9b0b3205f1b3fb00633a2f1ed82cbfb46e4f
kungpowjason <ywu4@wpi.edu>
2016-05-08 11:38:53 -0400

    Testing if the right user commit

--- FinalExamGradeCalculator/src/gradecalculator/model/Assignment.java
+++ FinalExamGradeCalculator/src/gradecalculator/model/Assignment.java
@@ -1,24 +1,36 @@
 package gradecalculator.model;
 
+/**
+ * Assignment keeps track of the percent earned from the assignment and the
+ * total worth of the assignment in the course.
+ * 
+ * @author ywu
+ *
+ */
 public class Assignment {
 	double pctgrade;
 	double pcttotalworth;
-	public Assignment(double pctgrade, double pcttotalworth){
+
+	public Assignment(double pctgrade, double pcttotalworth) {
 		this.pctgrade = pctgrade;
 		this.pcttotalworth = pcttotalworth;
 	}
+
 	/**
 	 * Getter for Percent Grade.
+	 * 
 	 * @return
 	 */
-	public double getPctGrade(){
+	public double getPctGrade() {
 		return pctgrade;
 	}
+
 	/**
 	 * Getter for Percent of the Total Worth.
+	 * 
 	 * @return
 	 */
-	public double getPctTotalWorth(){
+	public double getPctTotalWorth() {
 		return pcttotalworth;
 	}
 }
