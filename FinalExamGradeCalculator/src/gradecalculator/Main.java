143a14943ac4777655c57ac26fff7f8a36b766b2
kungpowjason <ywu4@wpi.edu>
2016-05-14 08:30:11 -0400

    Added JFreeCharts in the JFrame, moveable will modify later

--- FinalExamGradeCalculator/src/gradecalculator/Main.java
+++ FinalExamGradeCalculator/src/gradecalculator/Main.java
@@ -4,6 +4,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import gradecalculator.model.FEGCalculator;
+import gradecalculator.model.PieChart;
 import gradecalculator.view.Application;
 
 public class Main {
@@ -14,6 +15,12 @@ public class Main {
 		// if using 'app' within the anonymous class generated below, must be marked final.
 		final Application app = new Application(fegc);
 		app.setTitle("Grade Calculator");
+		
+		// Pie Chart Test
+		PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+        demo.pack();
+        demo.setVisible(true);
+        app.add(demo);
 
 		// state how to deal with leaving
 		app.addWindowListener(new WindowAdapter() {
